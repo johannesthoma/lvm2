e01220c86b5f918ebffdde3e4433012f1d485eb3
Johannes Khoshnazar-Thoma <johannes@johannesthoma.com>
2025-11-20 16:35:18 +0100

    Fixed a dummy function (get_dev_node_read_ahead) signature

--- libdm/libdm-common.c
+++ libdm/libdm-common.c
@@ -1443,7 +1443,8 @@ static int _set_dev_node_read_ahead(const char *dev_name,
 
 #else
 
-int get_dev_node_read_ahead(const char *dev_name, uint32_t *read_ahead)
+int get_dev_node_read_ahead(const char *dev_name, uint32_t major, uint32_t minor,
+			    uint32_t *read_ahead)
 {
 	*read_ahead = 0;
 
